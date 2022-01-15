a7928f825681a30b26419f4b394cb0e27e6c49c0
feliposz <feliposz@gmail.com>
2022-01-15 04:18:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Customer Website
+
+This project is a prototype of a customer website for the company I used to work in.
+
+I did this as a homework for an in company training on front end development.
+
+## Libraries used
+
+- jQuery v1.11.3
+- Bootstrap v3.3.6
---
+++ js/site.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+
+    var seta_cima = "▲";
+    var seta_baixo = "▼";
+
+    $(".quadro .collapse")
+        .each(function () {
+            var seta = $(this).hasClass("in") ? seta_cima : seta_baixo;
+            $(this).parent().find("h2 > a").append(' <span class="seta">' + seta + '</span>');
+        })
+        .on('hide.bs.collapse', function () {
+            $(this).parent().find('.seta').text(seta_baixo);
+        })
+        .on('show.bs.collapse', function () {
+            $(this).parent().find('.seta').text(seta_cima);
+        });
+
+    $(".navbar-nav li.menu").click(function () {
+        $(this).parent().find("li").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('#modalGenerico').on('show.bs.modal', function (event) {
+        var botao = $(event.relatedTarget);
+        var titulo = botao.data('title');
+        var modal = $(this);
+        var conteudo = botao.closest(".quadro").find(".collapse").clone();
+        if (conteudo.length != 0) {
+            conteudo.removeClass("collapse");
+        } else {
+            conteudo = botao.closest(".quadro").find(".row").clone();
+        }
+        modal.find('.modal-title').text(titulo);
+        modal.find('.modal-body').empty().append(conteudo);
+    })
+
+});
